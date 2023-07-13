5117c216317bd2d56718d25f1611bb500986146a
ansurfen <axf593161@gmail.com>
2023-07-13 23:47:51 +0800

    release 0.0.2

--- boot.lua
+++ boot.lua
@@ -1,25 +1,25 @@
 ---@diagnostic disable: undefined-global
-local defaultTarget = function(ver)
-    local tmpl = "https://github.com/ansurfen/ark/releases/download/{{.Ver}}/{{.Target}}"
-    local target = "ark"
-    if env.platform.OS == "windows" then
-        target = target .. ".zip"
-    else
-        target = target .. ".tar.gz"
-    end
-    return strf(tmpl, {
-        ver = ver,
-        target = target,
-    })
-end
-
-local defaultVersion = "0.0.1.1"
+local defaultVersion = "0.0.2"
 
 return {
     version = defaultVersion,
     name = "ark",
     load = function(opt)
-        local file = fetch.zip(opt.url or defaultTarget(opt.ver or defaultVersion))
-        uncompress(file, pathf(env.yock_path, "test/yock_modules/ark"))
+        local target = "ark"
+        if env.platform.OS == "windows" then
+            target = target .. ".zip"
+        else
+            target = target .. ".tar.gz"
+        end
+        local file = multi_fetch({
+            tag = target,
+            ver = opt.ver or defaultVersion
+        }, {
+            github = "https://github.com/ansurfen/ark/releases/download/{{.Version}}/{{.Target}}",
+            gitee = "https://gitee.com/ansurfen/ark/releases/download/{{.Version}}/{{.Target}}"
+        })
+        uncompress(pathf(env.yock_tmp, file), pathf(env.yock_path, "yock_modules"))
+        cd(pathf(env.yock_modules, "ark"))
+        sh("yock mount ark ctl.lua")
     end
 }
